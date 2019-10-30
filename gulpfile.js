f27e94b797d355d3af0babc468eb56700cb6d365
REDiowave <boschdoor.ian@gmail.com>
2019-10-30 14:28:13 -0700

    sass and broken js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,17 @@ const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 const eslint = require('gulp-eslint');
 const cleanCss = require('gulp-clean-css');
+const sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer');
+// Sass
+gulp.task('sass', () => gulp
+    .src('./sass/style.scss')
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+      })));
 // Lint
 gulp.task('lint', () => gulp
   .src('./js/*.js')
@@ -41,4 +52,4 @@ gulp.task('browser-sync', () => {
 gulp.task('watch', () => {
   gulp.watch(['js/*.js', 'css/*.css'], gulp.parallel(['mini-scripts', 'mini-styles']));
 });
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+gulp.task('default', gulp.parallel(['browser-sync', 'watch', 'sass']));
