a83e5d702b676cbf62f826cdad7ec07d1933f933
REDiowave <boschdoor.ian@gmail.com>
2019-11-03 13:07:15 -0800

    committing

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,76 @@
-// Variables
-const gulp = require('gulp');
-const terser = require('gulp-terser');
-const rename = require('gulp-rename');
-const browserSync = require('browser-sync').create();
-const eslint = require('gulp-eslint');
-const cleanCss = require('gulp-clean-css');
-const sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer');
+// Varibles
+const gulp = require("gulp"),
+  terser = require("gulp-terser"),
+  rename = require("gulp-rename"),
+  browserSync = require("browser-sync"),
+  eslint = require("gulp-eslint"),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cssnano = require('gulp-cssnano'),
+  prettyError = require('gulp-prettyerror');
+
 // Sass
-gulp.task('sass', () => gulp
-    .src('./sass/style.scss')
+gulp.task('sass', function () {
+  return gulp
+    .src('./css/styles.scss')
     .pipe(prettyError())
     .pipe(sass())
     .pipe(
-      autoprefixer({
-        browsers: ['last 2 versions'],
-      })));
+      autoprefixer(),
+    )
+    .pipe(gulp.dest('./build/css'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
 // Lint
-gulp.task('lint', () => gulp
-  .src('./js/*.js')
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError()));
-// JS format and minify
-gulp.task('mini-scripts', gulp.series('lint',
-  () => gulp
-    .src('./js/*.js')
-    .pipe(terser())
-    .pipe(rename({
-      extname: '.min.js',
-    }))
-    .pipe(gulp.dest('./build/js'))));
-// CSS minify
-gulp.task('mini-styles', () => gulp
-  .src('./css/*.css')
-  .pipe(cleanCss())
-  .pipe(rename({
-    extname: '.min.css',
-  }))
-  .pipe(gulp.dest('./build/css')));
-// Static server
-gulp.task('browser-sync', () => {
+gulp.task("lint", function () {
+  return gulp
+    .src("./js/*.js")
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+// Scripts
+gulp.task("scripts",
+  gulp.series("lint",
+    function scripts() {
+      return gulp
+        .src("./js/*.js")
+        .pipe(terser())
+        .pipe(rename({
+          extname: ".min.js"
+        }))
+        .pipe(gulp.dest("./build/js"));
+    }
+  )
+);
+
+
+gulp.task("say_hello", function (done) {
+  console.log("Hello!");
+  done();
+});
+
+/**
+ * Browser Sync
+ */
+gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
-      baseDir: './',
-    },
+      baseDir: "./"
+    }
   });
-  gulp.watch(['*.html', 'build/js/*.js', 'build/css/*.css'])
-    .on('change', browserSync.reload);
+
+  gulp.watch(["*.html", "./build/js/*.js", "./build/css/*.css"])
+    .on("change", browserSync.reload);
 });
-gulp.task('watch', () => {
-  gulp.watch(['js/*.js', 'css/*.css'], gulp.parallel(['mini-scripts', 'mini-styles']));
+
+gulp.task("watch", function () {
+  gulp.watch("./js/*.js", gulp.series("scripts"));
+  gulp.watch("./css/*.scss", gulp.series("sass"));
 });
-gulp.task('default', gulp.parallel(['browser-sync', 'watch', 'sass']));
+
+gulp.task("default", gulp.parallel("browser-sync", "watch"));
--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,68 @@
-! function (e) {
-  "use strict";
-  e(function () {
-    let a, t, i;
-    const s = e(".ajax-loader"),
-      n = e(".stories");
-    e("#sections").on("change", function () {
-      const l = e(this).val();
-      l.length && (i = "https://api.nytimes.com/svc/topstories/v2/" + l + ".json?api-key=0751ffff01d7a70710354972fa0ad4a9", n.empty(), a = "", t = "", e(".logo img").css({
-        height: "50%",
-        width: "50%"
-      }), e(".site-header").css({
-        "align-items": "flex-start",
-        flex: "1 0 auto",
-        height: "auto",
-        "max-width": "600px"
-      }), e(".search-placeholder").hide(), s.css("display", "block"), e.ajax({
-        method: "GET",
-        url: i,
-        dataType: "json"
-      }).done(function (i) {
-        let s, l, o;
-        if (0 !== (a = i.results).length) {
-          const i = a.filter(function (e) {
-            return e.multimedia.length;
-          }).slice(0, 12);
-          t += "<ul>", e.each(i, function (e, a) {
-            o = a.multimedia[4].url, l = a.abstract, s = a.url, t += '<li class="article-item">', t += '<a href="' + s + '" target="_blank">', t += '<div class="inner-item-wrapper">', t += '<div class="article" style="background-image:url(' + o + ')">', t += '<div class="story-meta">', t += "<p>" + (l || "This story has no description.") + "</p>", t += "</div>", t += "</div>", t += "</div>", t += "</a>", t += "</li>";
-          }), t += "</ul>";
-        } else t += '<p class="feedback">Sorry, nothing found! Please try again.</p>';
-        n.hide().fadeIn("fast").append(t);
-      }).fail(function () {
-        n.append('<p class="feedback">Sorry! There was a problem, please try again.</p>');
-      }).always(function () {
-        s.hide();
-      }));
-    });
+async function fetchStories() {
+  try {
+    // fetch the data
+    const data = await fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=cuqn1CNAXPPInZkmed1SYAtOGFewjKgB');
+    // parse the body
+    const json = await data.json();
+    // return the data
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+(async function () {
+  let stories = await fetchStories();
+
+  if(!stories) {
+    return;
+  }
+
+$('.ajax-loader').hide();
+  
+  let science = stories.results.filter(function (story) {
+    console.log('we\'re in filter');
+    if(story.multimedia.length <= 0) {
+      return;
+    }
+
+    const image = story.multimedia.filter(image => image.format === "Normal");
+    console.log(image[0].url);
+
+    if (!image) return;
+    
+    const title = story.title;
+
+    const html = `<div class="grid-item">
+      <h1>'${title}'</h1>
+      <img src='${image[0].url}'/>
+    </div>`;
+
+    $('.story-grid').append(html);
   });
-}(jQuery);
+  
+  // stories.results.forEach((story) => {
+  // $('.story-grid').append(`<div class="grid-item">${story.section}</div>`);
+  // });
+
+  // console.log(stories);
+
+
+// fetch('./data.json')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Work with JSON data here
+//     const data = JSON.parse(this.response);
+//     data.array.forEach(article => {
+//       console.log(article.title);
+//     });
+//     console.log(data);
+//   })
+//   .catch(err => {
+//     // Do something for an error here
+//   }); 
+})();
+
