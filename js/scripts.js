9cbf8186354a8b76e73b16318b256fb09758d2b0
Ian <55965026+IanDRED@users.noreply.github.com>
2019-11-03 20:46:41 -0800

    stuff done

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,44 @@
 async function fetchStories() {
-  try {
-    // fetch the data
-    const data = await fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=cuqn1CNAXPPInZkmed1SYAtOGFewjKgB');
-    // parse the body
-    const json = await data.json();
-    // return the data
-    return json;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        // fetch the data
+        const data = await fetch('https://api.nytimes.com/svc/topstories/v2/science.json?api-key=cuqn1CNAXPPInZkmed1SYAtOGFewjKgB');
+        // parse the body
+        const json = await data.json();
+        // return the data
+        return json;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 
-(async function () {
-  let stories = await fetchStories();
+(async function() {
+    let stories = await fetchStories();
 
-  if(!stories) {
-    return;
-  }
+    console.log(stories);
 
-$('.ajax-loader').hide();
-  
-  let science = stories.results.filter(function (story) {
-    // console.log('we\'re in filter');
-    if(story.multimedia.length <= 0) {
-      return;
+    if (!stories) {
+        return;
     }
 
-    const image = story.multimedia.filter(image => image.format === "Normal");
-    // console.log(image[0].url);
+    $('.ajax-loader').hide();
 
-    if (!image) return;
-    
-    const title = story.title;
+    let science = stories.results.filter(function(story) {
+        if (story.multimedia.length <= 0) {
+            return;
+        }
 
-    const html = `<div class="grid-item">
+        const image = story.multimedia.filter(image => image.format === "superJumbo");
+
+        if (!image) return;
+
+        const content = story.abstract;
+
+        const html = `<div class="grid-item">
       <img src='${image[0].url}'/>
-      <h1>'${title}'</h1>
+      <p>${content}</p>
     </div>`;
 
-    $('.story-grid').append(html);
-  });
-  
-  // stories.results.forEach((story) => {
-  // $('.story-grid').append(`<div class="grid-item">${story.section}</div>`);
-  // });
-
-  // console.log(stories);
-
-
-// fetch('./data.json')
-//   .then(response => {
-//     return response.json();
-//   })
-//   .then(data => {
-//     // Work with JSON data here
-//     const data = JSON.parse(this.response);
-//     data.array.forEach(article => {
-//       console.log(article.title);
-//     });
-//     console.log(data);
-//   })
-//   .catch(err => {
-//     // Do something for an error here
-//   }); 
+        $('.story-grid').append(html);
+    });
 })();
-
